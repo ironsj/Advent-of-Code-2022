96cc8c22f0450bd8205ac3e920b23eef22187579
ironsj <jakeirons007@gmail.com>
2022-12-13 02:54:27 -0500

    Adding day 12 part 1

---
+++ Day-12/Signal/Signal/Grid.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Signal
+{
+    public class Grid
+    {
+        public readonly int Columns;
+        public readonly int Rows;
+
+        public readonly Cell[,] MyGrid;
+        public readonly Cell Start;
+        public readonly Cell End;
+
+        private const char StartValue = 'S';
+        private const char EndValue = 'E';
+        private const char Lowest = 'a';
+        private const char Heighest = 'z';
+
+        private readonly Dictionary<Cell, List<Cell>> Neighbors = new();
+
+        public Grid(string[] input)
+        {
+            string[] lines = input.Select(line => line.Trim()).ToArray();
+            Columns = lines[0].Length;
+            Rows = lines.Length;
+
+            MyGrid = new Cell[Rows, Columns];
+
+            for (int row = 0; row < Rows; ++row)
+            {
+                for (int col = 0; col < Columns; ++col)
+                {
+                    Cell cell = new(col, row, lines[row][col]);
+
+                    MyGrid[row, col] = cell;
+
+                    if (cell.Value == StartValue)
+                    {
+                        Start = cell;
+                    }
+                    else if (cell.Value == EndValue)
+                    {
+                        End = cell;
+                    }
+                }
+            }
+
+            for (var row = 0; row < Rows; ++row)
+            {
+                for (var col = 0; col < Columns; ++col)
+                {
+                    var cell = MyGrid[row, col];
+
+                    var leftNeighbor = FindCell(row, col - 1);
+                    var rightNeighbor = FindCell(row, col + 1);
+                    var upNeighbor = FindCell(row - 1, col);
+                    var downNeighbor = FindCell(row + 1, col);
+
+                    var neighbors = new List<Cell?> { leftNeighbor, rightNeighbor, upNeighbor, downNeighbor, };
+
+                    Neighbors[cell] = neighbors
+                        .Where(neighbor => neighbor is not null && IsValid(cell, neighbor))
+                        .Select(neighbor => neighbor!)
+                        .ToList();
+                }
+            }
+        }
+
+        public List<Cell> GetNeighbors(Cell cell) => Neighbors[cell];
+
+        private Cell? FindCell(int row, int col)
+        {
+            return (row >= 0 && row < Rows && col >= 0 && col < Columns) ? MyGrid[row, col] : null;
+        }
+
+        private bool IsValid(Cell currentCell, Cell neighborCell)
+        {
+            if(currentCell.Value == StartValue)
+            {
+                return neighborCell.Value == Lowest;
+            }
+            else if(neighborCell.Value == EndValue)
+            {
+                return currentCell.Value == Heighest;
+            }
+
+            return neighborCell.Value - currentCell.Value <= 1;
+        }
+    }
+}
---
+++ Day-12/Signal/Signal/Program.cs
@@ -0,0 +1,79 @@
+﻿
+
+namespace Signal
+{
+    class SignalProgram
+    {
+        static void Main()
+        {
+            string[] input = File.ReadAllText(@"../../../../../input.txt").Split('\n');
+            long result = SolvePart1(input);
+            // long result = SolvePart2(input);
+            Console.WriteLine("Part 1: " + result);
+        }
+
+        public static int SolvePart1(string[] input)
+        {
+            Grid grid = new(input);
+            List<Cell> useBfs = BFS(grid, grid.Start, grid.End);
+            return useBfs.Count - 1;
+        }
+
+        public static int SolvePart2(string[] input)
+        {
+        }
+
+
+        public static List<Cell> BFS(Grid grid, Cell start, Cell end)
+        {
+            Dictionary<Cell, Cell?> bfsDiscovered = new()
+            {
+                { start, null }
+            };
+
+            Queue<Cell> bfsQueue = new Queue<Cell>();
+            bfsQueue.Enqueue(start);
+
+            while(bfsQueue.Count > 0)
+            {
+                Cell currentCell = bfsQueue.Dequeue();
+                
+                if(currentCell == end)
+                {
+                    break;
+                }
+
+                foreach (Cell neighbor in grid.GetNeighbors(currentCell))
+                {
+                    if (!bfsDiscovered.ContainsKey(neighbor))
+                    {
+                        bfsDiscovered.Add(neighbor, currentCell);
+                        bfsQueue.Enqueue(neighbor);
+                    }
+                }
+
+            }
+
+            List<Cell> path = GetPath(grid.Start, grid.End, bfsDiscovered);
+
+            
+            return path;
+        }
+
+        public static List<Cell> GetPath(Cell start, Cell end, Dictionary<Cell, Cell?> discovered)
+        {
+            List<Cell> path = new List<Cell>();
+            Cell currentCell = end;
+            while (currentCell != start)
+            {
+                path.Add(currentCell);
+                currentCell = discovered[currentCell]!;
+            }
+            path.Add(start);
+            path.Reverse();
+            return path;
+        }
+
+
+    }
+}
