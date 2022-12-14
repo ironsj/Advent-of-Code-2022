edd84638d0030305795caf489e49d3b33c0e7dc9
ironsj <jakeirons007@gmail.com>
2022-12-14 00:33:55 -0500

    Adding everything for day 14

---
+++ Day-14/Sand/Sand/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Nodes;
+
+namespace Sand
+{
+    class SandProgram
+    {
+        static void Main()
+        {
+            string[] input = File.ReadAllText(@"../../../../../input.txt").Split('\n');
+            int result = SolvePart1(input);
+            int result2 = SolvePart2(input);
+            Console.WriteLine("Part 1: " + result);
+            Console.WriteLine("Part 2: " + result2);
+        }
+
+        public static int SolvePart1(string[] input)
+        {
+            return 1;
+        }
+
+        public static int SolvePart2(string[] input)
+        {
+            return 1;
+        }
+    }
+}
