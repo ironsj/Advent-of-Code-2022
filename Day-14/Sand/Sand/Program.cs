58b36c5ea08c8eceabb6b5f78692b26ecf04062b
ironsj <jakeirons007@gmail.com>
2022-12-14 22:05:54 -0500

    Adding day 14 part 2

--- Day-14/Sand/Sand/Program.cs
+++ Day-14/Sand/Sand/Program.cs
@@ -5,6 +5,7 @@ namespace Sand
     class SandProgram
     {
         private static HashSet<(int x, int y)> Wall { get; set; } = new();
+        private static int Floor { get; set; }
         static void Main()
         {
             string[] input = File.ReadAllText(@"../../../../../input.txt").Split('\n');
@@ -31,7 +32,21 @@ namespace Sand
 
         public static int SolvePart2(string[] input)
         {
-            return 1;
+            // clear the HashSet for the second part
+            Wall.Clear();
+            Initialize(input);
+
+            // the heighest y coordinate plus 1 (1 unit above the floor where we are standing)
+            Floor = Wall.Max(wall => wall.y) + 1;
+
+            int droppedSand = 0;
+            // loop while sand can still drop (the source is not covered)
+            while (CanDropSand())
+            {
+                droppedSand++;
+            }
+
+            return droppedSand;
         }
 
         public static bool Stop((int x, int y) pos)
@@ -39,6 +54,25 @@ namespace Sand
             // count the number of walls with a y coordinate greater than/equal to the y position of the current position
             // if this equals 1 this means we are falling into the abyss as nothing will be able to stop it after this point
             return Wall.Count(m => m.y >= pos.y) == 1;
+        }
+
+        public static bool Stop2((int x, int y) pos)
+        {
+            // stop if the new y position is equal to the floor of the cave
+            return pos.y == Floor;
+        }
+
+        public static bool CanDropSand()
+        {
+            // if the source is in the Walls (covered) return false
+            if (Wall.Contains((500, 0)))
+            {
+                return false;
+            }
+            // drop from the source
+            Wall.Add((500, 0));
+            MoveSand2((500, 0));
+            return true;
         }
 
         public static void Initialize(string[] input)
@@ -124,5 +158,52 @@ namespace Sand
             // if all of spots checked above are blocked, return true and drop a new piece of sand
             return true;
         }
+
+        public static bool MoveSand2((int x, int y) pos)
+        {
+            // if the current y position is the floor return false
+            if (Stop2((pos.x, pos.y)))
+            {
+                return false;
+            }
+
+            // check if the position below the current position is blocked
+            if (!Wall.Contains((pos.x, pos.y + 1)))
+            {
+                // remove the current position from wall as it is not blocked
+                Wall.Remove((pos.x, pos.y));
+                // add the position below
+                Wall.Add((pos.x, pos.y + 1));
+                // call again for the position below
+                return MoveSand2((pos.x, pos.y + 1));
+            }
+
+            // check if the spot diagonally left is blocked
+            if (!Wall.Contains((pos.x - 1, pos.y + 1)))
+            {
+                // remove current position
+                Wall.Remove((pos.x, pos.y));
+                // add the position diagonally to the left
+                Wall.Add((pos.x - 1, pos.y + 1));
+                // call for the next position
+                return MoveSand2((pos.x - 1, pos.y + 1));
+            }
+
+            // check if the spot diagonally right is blocked
+            if (!Wall.Contains((pos.x + 1, pos.y + 1)))
+            {
+                // remove current position
+                Wall.Remove((pos.x, pos.y));
+                // add the position diagonally right
+                Wall.Add((pos.x + 1, pos.y + 1));
+                // call for the next position
+                return MoveSand2((pos.x + 1, pos.y + 1));
+            }
+
+            // if all of spots checked above are blocked, return true and drop a new piece of sand
+            return true;
+        }
     }
+
+
 }
