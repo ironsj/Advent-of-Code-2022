ae78732be38d727cf7b7e0d350629db34869dfca
ironsj <jakeirons007@gmail.com>
2022-12-16 22:06:48 -0500

    Adding day 15 part 2

--- Day-15/Sensors/Sensors/Program.cs
+++ Day-15/Sensors/Sensors/Program.cs
@@ -8,9 +8,9 @@ namespace Sensors
         private static readonly Dictionary<string, Beacon> Beacons = new();
         static void Main()
         {
-            string[] input = File.ReadAllText(@"../../../../../input.txt").Split('\n');
+            string[] input = File.ReadAllText(@"../../../../../input_example.txt").Split('\n');
             long result = SolvePart1(input);
-            int result2 = SolvePart2(input);
+            long result2 = SolvePart2();
             Console.WriteLine("Part 1: " + result);
             Console.WriteLine("Part 2: " + result2);
         }
@@ -72,12 +72,89 @@ namespace Sensors
             return invalidPositionCount;
         }
 
-        public static int SolvePart2(string[] input)
+        public static long SolvePart2()
         {
+            List<Sensor> sensorsList = Sensors.Values.ToList();
+            var edgeCoordinates = sensorsList.Select(sensor => GetEdgeCoordinates(sensor)).SelectMany(coordinate => coordinate);
+
+            // iterate through each coordinate that is on the perimeter of the sensor ranges
+            foreach (Coordinate coordinate in edgeCoordinates)
+            {
+                // CHANGE 4000000 TO 20 FOR PROPER RESULTS FOR EXAMPLE INPUT
+                // make sure the coordinate is in the range of the distress beacon
+                if (coordinate.X > 0 && coordinate.X < 20 && coordinate.Y > 0 && coordinate.Y < 20)
+                {
+                    // check if the positions's distance to the sensor is greater than a sensor to it's closest beacon for each sensor
+                    // find a position that is not in any sensor ranges
+                    bool inSensorRange = sensorsList.Any(sensor => sensor.DistanceToBeacon >= sensor.DistanceTo(coordinate));
+
+                    // if the distance to a coordinate is greater than than beacon distance for each sensor we return below
+                    if (!inSensorRange)
+                    {
+                        return coordinate.X * 4000000 + coordinate.Y;
+                    }
+                }
+            }
+            
             return 1;
         }
 
-        private static Beacon ParseBeacon(string key)
+        private static List<Coordinate> GetEdgeCoordinates(Sensor sensor)
+        {
+            // find the range of the sensor + 1 for manhattan distance
+            // furthest left in sensor range plus 1
+            Coordinate left = new(sensor.Position.X - sensor.DistanceToBeacon - 1, sensor.Position.Y);
+            //furthest right in sensor range plus 1
+            Coordinate right = new(sensor.Position.X + sensor.DistanceToBeacon + 1, sensor.Position.Y);
+            // furthest up in sensor range plus 1
+            Coordinate top = new(sensor.Position.X, sensor.Position.Y + sensor.DistanceToBeacon + 1);
+            // furthest down in sensor range plus
+            Coordinate bottom = new(sensor.Position.X, sensor.Position.Y - sensor.DistanceToBeacon - 1);
+
+            // list of the coordinates of the perimeter of the range
+            List<Coordinate> edges = new();
+
+            // get the edges for the sensor range and add them to the lsit
+            edges = edges
+                .Concat(GetCoordinatesBetween(left, top))
+                .Concat(GetCoordinatesBetween(bottom, right))
+                .Concat(GetCoordinatesBetween(top, right))
+                .Concat(GetCoordinatesBetween(left, bottom))
+                .ToList();
+
+            return edges;
+        }
+
+        private static IEnumerable<Coordinate> GetCoordinatesBetween(Coordinate start, Coordinate end)
+        {
+            // determine which coordinate betweent the start and end is the left and right
+            var (left, right) = start.X <= end.X ? (start, end) : (end, start);
+            //holds all coordinates between the start and end
+            List<Coordinate> coordinates = new();
+
+            // check if the left coordinate is higher or lower than the right coordinate
+            if (left.Y <= right.Y)
+            {
+                // add the coordinates between the left and right coordinates
+                for (long offset = 0; offset <= right.X - left.X; offset++)
+                {
+                    coordinates.Add(new Coordinate(left.X + offset, left.Y + offset));
+                }
+            }
+            else
+            {
+                // add the coordinates between the left and right coordinates
+                for (long offset = 0; offset <= right.X - left.X; offset++)
+                {
+                    coordinates.Add(new Coordinate(left.X + offset, left.Y - offset));
+                }
+            }
+
+            // return the list of coordinates between the two positions
+            return coordinates;
+        }
+
+    private static Beacon ParseBeacon(string key)
         {
             // check if beacon is already in the dictionary
             if (!Beacons.ContainsKey(key))
