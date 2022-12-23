d2ba75cc50fe53e732952ac3d69646ea97ea794f
ironsj <jakeirons007@gmail.com>
2022-12-23 00:03:40 -0500

    Adding day 22 part 2

--- Day-22/password/src/main.rs
+++ Day-22/password/src/main.rs
@@ -169,6 +169,78 @@ fn wrap(map: &[Vec<Tile>], pos: &Coord, dir: &Direction) -> Coord {
     curr
 }
 
+fn wrap2(pos: &Coord, dir: &Direction) -> (Coord, Direction) {
+    // if we are to divide our input into 50x50 grids we have something as follows (* indicates 50x50 grid):
+    // * * *
+    // * * *
+    // * * *
+    // * * *
+    // Therefore, all rows are 0..150 and all columns are 0..200
+    // Our cubes are located in:
+    // * # #
+    // * # *
+    // # # *
+    // # * *
+    // Thus we will index the cubes (e.g. farthest top and left cub is 0,1) and hard code what cube and direction they end up in as follows:
+    // P.S. this is not a comprehensive list, but it covers all of the cases you would leave the solid/open tiles
+    let (cube_row, cube_col, new_dir) = match (pos.row / 50, pos.col / 50, dir) {
+        (0, 1, Direction::L) => (2, 0, Direction::R),
+        (0, 1, Direction::U) => (3, 0, Direction::R),
+        (0, 2, Direction::U) => (3, 0, Direction::U),
+        (0, 2, Direction::R) => (2, 1, Direction::L),
+        (0, 2, Direction::D) => (1, 1, Direction::L),
+        (1, 1, Direction::L) => (2, 0, Direction::D),
+        (1, 1, Direction::R) => (0, 2, Direction::U),
+        (2, 0, Direction::U) => (1, 1, Direction::R),
+        (2, 0, Direction::L) => (0, 1, Direction::R),
+        (2, 1, Direction::R) => (0, 2, Direction::L),
+        (2, 1, Direction::D) => (3, 0, Direction::L),
+        (3, 0, Direction::L) => (0, 1, Direction::D),
+        (3, 0, Direction::R) => (2, 1, Direction::U),
+        (3, 0, Direction::D) => (0, 2, Direction::D),
+        _ => unreachable!(),
+    };
+
+    // get our index within the current cube
+    let (row_index, column_index) = (pos.row % 50, pos.col % 50);
+
+    let i = match dir {
+        Direction::L => 49 - row_index,
+        Direction::R => row_index,
+        Direction::U => column_index,
+        Direction::D => 49 - column_index,
+    };
+
+    // find the row index in the new cube
+    let new_row = match new_dir {
+        Direction::L => 49 - i,
+        Direction::R => i,
+        // if you enter a cube going up, you are in row 49
+        Direction::U => 49,
+        // if you enter a cube going down, you are in row 0
+        Direction::D => 0,
+    };
+    //find the column index in the new cube
+    let new_col = match new_dir {
+        // if you enter a cube going left, you are in column 49
+        Direction::L => 49,
+        // if you enter a cube going right, you are in column 0
+        Direction::R => 0,
+        Direction::U => i,
+        Direction::D => 49 - i,
+    };
+
+    // find the new position
+    // multiply the cube (row, col) by 50 to add the offset for the row,col in the cube
+    let new_pos = Coord {
+        row: cube_row * 50 + new_row,
+        col: cube_col * 50 + new_col,
+    };
+
+    // return the new position and direction
+    (new_pos, new_dir)
+}
+
 fn part_1(input: &str) -> i32 {
     // parse the input and get our grid and instructions
     let (map, instructions) = parse(input);
@@ -240,7 +312,74 @@ fn part_1(input: &str) -> i32 {
 }
 
 fn part_2(input: &str) -> i32 {
-    return 1;
+    // parse the input and get our grid and instructions
+    let (map, instructions) = parse(input);
+    // find the index of the first open position in the first row
+    let start_col = map[0].iter().position(|tile| *tile == Tile::Open).unwrap() as i32;
+
+    // set our current position
+    let mut pos = Coord {
+        row: 0,
+        col: start_col,
+    };
+    // set our current direction
+    let mut dir = Direction::R;
+
+    // go through each instruction
+    for inst in &instructions {
+        // match the instruction
+        match inst {
+            // if it's a rotation, rotate our direction
+            Instruction::Rotate(turn) => dir = dir.turn(&turn),
+            // if it's a forward instruction, move forward
+            Instruction::Forward(num) => {
+                // repeat num times
+                for _ in 0..*num {
+                    // get the offset of our current direction (e.g. facing right, offset is (0, 1) from current position)
+                    let Coord {
+                        row: row_offset,
+                        col: col_offset,
+                    } = dir.offset();
+
+                    // get the tile at the new position
+                    let new_tile = map
+                        .get((pos.row + row_offset) as usize)
+                        .and_then(|row| row.get((pos.col + col_offset) as usize))
+                        .unwrap_or(&Tile::None);
+
+                    // match the new tile
+                    match new_tile {
+                        // if new tile is solid, we stop moving in this direction and go to next instruction
+                        Tile::Solid => break,
+                        // if new tile is open, we move there
+                        Tile::Open => {
+                            pos = Coord {
+                                row: pos.row + row_offset,
+                                col: pos.col + col_offset,
+                            };
+                        }
+                        // if new tile is not found, wrap around
+                        Tile::None => {
+                            // get the position and direction in the new 50x50 cube
+                            let (new_pos, new_dir) = wrap2(&pos, &dir);
+
+                            // if the position in the new cube is a wall, we stop moving in this direction and go to next instruction
+                            if map[new_pos.row as usize][new_pos.col as usize] == Tile::Solid {
+                                break;
+                            }
+                            // otherwise we move to the position in the new cube and update the direction
+                            pos = new_pos;
+                            dir = new_dir;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // return the score
+    // (adding 1 to row and col because index starts at 1)
+    1000 * (pos.row + 1) + 4 * (pos.col + 1) + dir.score() as i32
 }
 
 fn main() {
