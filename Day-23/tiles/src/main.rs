2c8cf6783b6abdd7f6f4348af6c4a9624407b1d4
ironsj <jakeirons007@gmail.com>
2022-12-23 19:21:49 -0500

    Adding day 23 part 2

--- Day-23/tiles/src/main.rs
+++ Day-23/tiles/src/main.rs
@@ -177,7 +177,9 @@ fn part_1(input: &str) -> usize {
         if round == 10 {
             // get the min and max row and column of the elves
             let (minmax_row, minmax_col) = elves.iter().fold(
+                // initialize min and max row and column to the max and min values of i32
                 ((i32::MAX, i32::MIN), (i32::MAX, i32::MIN)),
+                // for each elf, update the min and max row and column
                 |(minmax_row, minmax_col), Coord { row, col }| {
                     (
                         (minmax_row.0.min(*row), minmax_row.1.max(*row)),
@@ -202,7 +204,73 @@ fn part_1(input: &str) -> usize {
 }
 
 fn part_2(input: &str) -> i32 {
-    return 1;
+    // get all of the elves from the input
+    let mut elves = parse(input);
+    // the four directions the elves can propose in order
+    let mut checks = [
+        Direction::North,
+        Direction::South,
+        Direction::West,
+        Direction::East,
+    ];
+
+    for round in 1.. {
+        // the moved boolean changes to true if an elf moves. if no elves move at the end of a round, we have reached our final round
+        let mut moved = false;
+        // the key is the proposed coordinate and the value is a list of elves coordinates that propose going to it
+        let mut proposals: HashMap<Coord, Vec<Coord>> = HashMap::new();
+
+        // PHASE ONE: Consider 8 positions
+        for elf in &elves {
+            // get the eight neighbors of the elf
+            let neighbors = elf.neighbors();
+            // check if all of the neighbors are empty. if so, the elf doesn't do anything this round
+            if neighbors.iter().all(|coord| !elves.contains(coord)) {
+                continue;
+            }
+
+            // get a list of booleans that indicate if the neighbor is occupied
+            let neighbors = neighbors
+                .iter()
+                .map(|neighbour| elves.contains(neighbour))
+                .collect::<Vec<_>>()
+                .try_into()
+                .unwrap();
+
+            // check if the elf can move in any of the four directions, returns the first that it can propose
+            let proposed_dir = checks.iter().find(|dir| dir.check(&neighbors));
+            // if the elf can move in a direction
+            if let Some(dir) = proposed_dir {
+                // get the coordinate the elf would move to in the proposed direction
+                let proposal = elf.add_dir(dir);
+                // add the elf to the list of elves that would move to the proposed coordinate
+                proposals.entry(proposal).or_default().push(*elf);
+            }
+        }
+
+        // PHASE TWO: Move
+        for (new_coord, old_coords) in proposals {
+            // if only one elf would move to the proposed coordinate, move the elf
+            if old_coords.len() == 1 {
+                // an elf has moved, change the moved boolean to true
+                moved = true;
+                // remove the old coordinate from the HashSet
+                elves.remove(&old_coords[0]);
+                // add the new coordinate to the HashSet
+                elves.insert(new_coord);
+            }
+        }
+
+        // round finished, if no elves moved return the current round
+        if !moved {
+            return round;
+        }
+
+        // move the first direction to the end of the list (first round North moves to back, second round South moves to back, etc.)
+        checks.rotate_left(1);
+    }
+
+    i32::MAX
 }
 
 fn main() {
